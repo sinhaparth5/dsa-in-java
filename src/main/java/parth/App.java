4ff5d505d4e87e23f158636bd7fc12ccb41f891c
Parth Sinha <sinhaparth555@gmail.com>
2024-12-28 19:48:41 +0000

    added array in the java

---
+++ src/main/java/parth/App.java
@@ -0,0 +1,10 @@
+package parth;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/parth/array/ArrayUtil.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package parth.array;
+
+/**
+ *
+ * @author parth
+ */
+public class ArrayUtil {
+    public void printArray(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void arrayDemo() {
+        int[] myArray = new int[5];
+        // printArray(myArray);
+        myArray[0] = 10;
+        myArray[1] = 20;
+        myArray[2] = 30;
+        myArray[3] = 40;
+        myArray[4] = 50;
+        printArray(myArray);
+    }
+
+    public static void main(String[] args) {
+        ArrayUtil arrUtil = new ArrayUtil();
+        arrUtil.arrayDemo();
+    }
+}
